ee4daed372b6915fe7d48a43c330dca0ac96324d
WisleyASousa <wisleya.sousa@outlook.com.br>
2022-11-21 00:44:56 -0300

    melhorando vizualizaçao do cardapio

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -9,7 +9,7 @@ function Table(props: TableProps) {
     return props.dish?.map((dish) => {
       return (
         <>
-          <p>{dish.dish}</p>
+          <span>{dish.dish}</span>
         </>
       )
     })
@@ -18,86 +18,115 @@ function Table(props: TableProps) {
     return props.dish?.map((dish) => {
       return (
         <>
-          <p>{dish.day}</p>
+          <span>{dish.day}</span>
         </>
       )
     })
   }
 
   return (
-    <div>
-      <div className="container rounded-4 shadow p-3 mb-5 bg-body border d-flex flex-column gap-5 text-center">
-        <div className="row  border-bottom  p-2 text-start ">
-          <div className="col border-end">Dia</div>
-          <div className="col-8 border-end">Prato</div>
-          <div className="col">Ação</div>
-        </div>
-
-        <div className="row p-2  border-bottom  text-start ">
-          <div className="col text-nowrap  border-end">{renderDays()}</div>
-          <div className="col-8 border-end">{renderDishes()}</div>
-          <div className="col d-flex justify-content-evenly">
-            <button type="button" className="btn btn-info btn-sm">
-              <i className="bi bi-pencil-square"></i>
-            </button>
-            <button type="button" className="btn btn-danger btn-sm">
-              <i className="bi bi-trash3"></i>
-            </button>
-          </div>
-        </div>
-        <div className="row p-2  border-bottom  text-start ">
-          <div className="col border-end">Terça-feira</div>
-          <div className="col-8 border-end">Vazio</div>
-          <div className="col d-flex justify-content-evenly">
-            <button type="button" className="btn btn-info btn-sm">
-              <i className="bi bi-pencil-square"></i>
-            </button>
-            <button type="button" className="btn btn-danger btn-sm">
-              <i className="bi bi-trash3"></i>
-            </button>
-          </div>
-        </div>
-        <div className="row p-2  border-bottom  text-start ">
-          <div className="col border-end">Quarta-feira</div>
-          <div className="col-8 border-end">Vazio</div>
-          <div className="col d-flex justify-content-evenly">
-            <button type="button" className="btn btn-info btn-sm">
-              <i className="bi bi-pencil-square"></i>
-            </button>
-            <button type="button" className="btn btn-danger btn-sm">
-              <i className="bi bi-trash3"></i>
-            </button>
-          </div>
-        </div>
-        <div className="row p-2  border-bottom  text-start ">
-          <div className="col border-end">Quinta-feira</div>
-          <div className="col-8 border-end">Vazio</div>
-          <div className="col d-flex justify-content-evenly">
-            <button type="button" className="btn btn-info btn-sm">
-              <i className="bi bi-pencil-square"></i>
-            </button>
-            <button type="button" className="btn btn-danger btn-sm">
-              <i className="bi bi-trash3"></i>
-            </button>
-          </div>
-        </div>
-        <div className="row p-2 border-bottom text-start ">
-          <div className="col border-end">Sexta-feira</div>
-          <div className="col-8 border-end">Vazio</div>
-          <div className="col d-flex justify-content-evenly">
-            <button type="button" className="btn btn-info btn-sm">
-              <i className="bi bi-pencil-square"></i>
-            </button>
-            <button type="button" className="btn btn-danger btn-sm">
-              <i className="bi bi-trash3"></i>
-            </button>
-          </div>
-        </div>
-        <div className="col d-flex justify-content-end ">
-          <button type="button" className="btn btn-primary shadow-sm ">
-            <i className="bi bi-plus-lg h6"> Adicionar</i>
-          </button>
-        </div>
+    <div className="table-responsive rounded">
+      <table className="table caption-top shadow-sm text-nowrap table-bordered text-start table-hover ">
+        <thead>
+          <tr className="table-dark ">
+            <th scope="col" className="col-2 ">
+              Dia
+            </th>
+            <th scope="col" className="col-8">
+              Prato
+            </th>
+            <th scope="col" className="text-center">
+              Ação
+            </th>
+          </tr>
+        </thead>
+        <tbody className="">
+          <tr>
+            <th className="bg-primary text-dark bg-opacity-10" scope="row">
+              {renderDays()}
+            </th>
+            <td>{renderDishes()}</td>
+            <td>
+              <div className="col d-flex justify-content-evenly">
+                <button type="button" className="btn btn-info btn-sm">
+                  <i className="bi bi-pencil-square"></i>
+                </button>
+                <button type="button" className="btn btn-danger btn-sm">
+                  <i className="bi bi-trash3"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th className="bg-primary text-dark bg-opacity-10" scope="row">
+              Terça-feira
+            </th>
+            <td>Vazio</td>
+            <td>
+              <div className="col d-flex justify-content-evenly">
+                <button type="button" className="btn btn-info btn-sm">
+                  <i className="bi bi-pencil-square"></i>
+                </button>
+                <button type="button" className="btn btn-danger btn-sm">
+                  <i className="bi bi-trash3"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th className="bg-primary text-dark bg-opacity-10" scope="row">
+              Quarta-feira
+            </th>
+            <td>Vazio</td>
+            <td>
+              <div className="col d-flex justify-content-evenly">
+                <button type="button" className="btn btn-info btn-sm">
+                  <i className="bi bi-pencil-square"></i>
+                </button>
+                <button type="button" className="btn btn-danger btn-sm">
+                  <i className="bi bi-trash3"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th className="bg-primary text-dark bg-opacity-10" scope="row">
+              Quinta-feira
+            </th>
+            <td>Vazio</td>
+            <td>
+              <div className="col d-flex justify-content-evenly">
+                <button type="button" className="btn btn-info btn-sm">
+                  <i className="bi bi-pencil-square"></i>
+                </button>
+                <button type="button" className="btn btn-danger btn-sm">
+                  <i className="bi bi-trash3"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th className="bg-primary text-dark bg-opacity-10" scope="row">
+              Sexta-feira
+            </th>
+            <td>Vazio</td>
+            <td>
+              <div className="col d-flex justify-content-evenly">
+                <button type="button" className="btn btn-info btn-sm">
+                  <i className="bi bi-pencil-square"></i>
+                </button>
+                <button type="button" className="btn btn-danger btn-sm">
+                  <i className="bi bi-trash3"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="col d-flex justify-content-end ">
+        <button type="button" className="btn btn-primary shadow-sm ">
+          <i className="bi bi-plus-lg h6"> Adicionar</i>
+        </button>
       </div>
     </div>
   )
